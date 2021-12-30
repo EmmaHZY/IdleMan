d4d0f787317e02044836692e3c4d5af50fadd532
EmmaHZY <80582278+EmmaHZY@users.noreply.github.com>
2021-12-30 16:06:50 +0800

    搜索任务界面

--- IdleMan/app/src/main/java/com/example/idleman/Activity_Task_Check.java
+++ IdleMan/app/src/main/java/com/example/idleman/Activity_Task_Check.java
@@ -8,7 +8,9 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -19,7 +21,9 @@ import com.alibaba.fastjson.JSONObject;
 import connectHelper.OkHttpConnectHelper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Activity_Task_Check extends AppCompatActivity {
     private List<TaskItem> list = new ArrayList<>();
@@ -117,6 +121,142 @@ public class Activity_Task_Check extends AppCompatActivity {
             outRect.bottom = space;
             outRect.top = space;
         }
-
     }
+
+    //弹窗处理
+    Handler handler1 = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            new AlertDialog.Builder(
+                    Activity_Task_Check.this)
+                    .setMessage(msg.getData().getString("key"))
+                    .setPositiveButton("确定", null)
+                    .show();
+            return false;
+        }
+    });
+
+    //按tag搜索
+    public void research_tag(View view){
+        //获取用户输入
+        EditText tag = (EditText) findViewById(R.id.research_tag);
+        String Tag=tag.getText().toString();
+
+        String url="http://1.117.239.54:8080/task?operation=getByTag&index="+Data.getId()+"&key="+Tag;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = OkHttpConnectHelper.getTargetData(url);
+                JSONObject temp = JSON.parseObject(result);//结果转化为json对象
+                String judge = temp.getJSONObject("meta").getString("status");//获取状态码
+                if(!judge.equals("2011"))//没有东西
+                {
+                    //创建Message
+                    Message msg = Message.obtain();
+                    msg.what = 0;
+                    //创建Bundle
+                    Bundle bundle = new Bundle();
+                    bundle.putString("key","无相关信息，请核对后再输入");
+                    //为Message设置Bundle数据
+                    msg.setData(bundle);
+                    //发送消息
+                    handler1.sendMessage(msg);
+                    return;
+                }
+                JSONArray array = temp.getJSONArray("data");
+                taskCount = array.size();
+                for (int i = 0; i < taskCount; i++) {
+
+                    int id = 1;
+                    Log.d("task id", "task is NO." + id);
+                    taskText[i] = JSON.parseObject(array.getString(i)).getString("taskTitle");
+                    taskID[i] = JSON.parseObject(array.getString(i)).getLong("taskID");
+                    usernames[i] = JSON.parseObject(array.getString(i)).getString("username");
+                    taskTag[i] = JSON.parseObject(array.getString(i)).getString("tag");
+                }
+                String data = temp.getJSONArray("data").getString(0);//数组第一个元素的字符串值
+                JSONObject show = JSON.parseObject(data);//转化为json对象
+                list = new ArrayList<>();
+                for (int i = 0; i < taskCount; i++) {
+                    String text = taskText[i];
+                    Long id = taskID[i];
+                    String username = usernames[i];
+                    String tag = taskTag[i];
+                    list.add(new TaskItem(text, id, username, tag));
+                }
+                Message msg = Message.obtain();
+                handler.sendMessage(msg);
+            }
+        }).start();
+    }
+
+    //按tag搜索
+    public void research_id(View view){
+        //获取用户输入
+        EditText tag = (EditText) findViewById(R.id.research_tag);
+        String Tag=tag.getText().toString();
+        if(Tag.equals(Data.getId()))
+        {
+            //创建Message
+            Message msg = Message.obtain();
+            msg.what = 0;
+            //创建Bundle
+            Bundle bundle = new Bundle();
+            bundle.putString("key","无法在此处查询自己发布的任务，请转至~我的~界面");
+            //为Message设置Bundle数据
+            msg.setData(bundle);
+            //发送消息
+            handler1.sendMessage(msg);
+            return;
+        }
+
+        String url="http://1.117.239.54:8080/task?operation=getByOtherPublisherID&index="+Tag+"&key=";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = OkHttpConnectHelper.getTargetData(url);
+                JSONObject temp = JSON.parseObject(result);//结果转化为json对象
+                String judge = temp.getJSONObject("meta").getString("status");//获取状态码
+                if(!judge.equals("2011"))//没有东西
+                {
+                    //创建Message
+                    Message msg = Message.obtain();
+                    msg.what = 0;
+                    //创建Bundle
+                    Bundle bundle = new Bundle();
+                    bundle.putString("key","无相关信息，请核对后再输入");
+                    //为Message设置Bundle数据
+                    msg.setData(bundle);
+                    //发送消息
+                    handler1.sendMessage(msg);
+                    return;
+                }
+                JSONArray array = temp.getJSONArray("data");
+                taskCount = array.size();
+                for (int i = 0; i < taskCount; i++) {
+
+                    int id = 1;
+                    Log.d("task id", "task is NO." + id);
+                    taskText[i] = JSON.parseObject(array.getString(i)).getString("taskTitle");
+                    taskID[i] = JSON.parseObject(array.getString(i)).getLong("taskID");
+                    usernames[i] = JSON.parseObject(array.getString(i)).getString("username");
+                    taskTag[i] = JSON.parseObject(array.getString(i)).getString("tag");
+                }
+                String data = temp.getJSONArray("data").getString(0);//数组第一个元素的字符串值
+                JSONObject show = JSON.parseObject(data);//转化为json对象
+                list = new ArrayList<>();
+                for (int i = 0; i < taskCount; i++) {
+                    String text = taskText[i];
+                    Long id = taskID[i];
+                    String username = usernames[i];
+                    String tag = taskTag[i];
+                    list.add(new TaskItem(text, id, username, tag));
+                }
+                Message msg = Message.obtain();
+                handler.sendMessage(msg);
+            }
+        }).start();
+    }
+
+
 }
